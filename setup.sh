6c7f6ebb1e0e87c0c8f9d8c1e64522d9c5bf48e2
Patrick Humpal <654902+phumpal@users.noreply.github.com>
2024-03-15 17:02:11 +0000

    hail mary full of grace stow vundle in its place

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ sudo apt-get install -y stow silversearcher-ag
 if stow --dir=files --target="$HOME" $(ls files | sed 's#/##' | paste -sd " " -); then
   echo "ok"
 fi
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/plugin/Vundle.vim
+vim +PluginInstall +qall
