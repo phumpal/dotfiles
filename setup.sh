1001755755a19fdfd7b9303220c583a758d80003
Patrick Humpal <654902+phumpal@users.noreply.github.com>
2024-05-25 03:30:32 +0000

    update setup for gitconfig + vim

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y -qq stow silversearcher-ag
 
+if [ -f ~/.gitconfig ]; then
+  if [ -L ~/.gitconfig ]; then
+    unlink ~/.gitconfig
+  else
+    rm ~/.gitconfig
+  fi
+fi
+
 if stow --dir=files --target="$HOME" $(ls files | sed 's#/##' | paste -sd " " -); then
   echo "ok"
 fi
 
-if [ ! -d ~/.vim/plugin/Vundle.vim ]; then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  mkdir -p ~/.vim/bundle
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
