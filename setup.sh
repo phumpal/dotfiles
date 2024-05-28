3249aca02b2a57b0b064551f6a700664b8169797
Patrick Humpal <654902+phumpal@users.noreply.github.com>
2024-05-28 21:38:53 +0000

    vim: silence plugin install

--- setup.sh
+++ setup.sh
@@ -19,4 +19,4 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-vim +PluginInstall +qall
+vim +'PluginInstall --sync' +qall &> /dev/null
